e3be75b208eb3fc7f3e36afa4423a547a56cdf76
Nur_Alislam_Kastiro <22331549@studentmail.ul.ie>
2025-03-27 12:59:34 +0000

    added envs for angular \n\n co-author:Mykola Vaskevych

---
+++ frontend/src/env/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiHost: 'https://api.productiondomain.com' // Production API URL
+};
---
+++ frontend/src/env/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiHost: 'http://localhost:8000', // Development API URL
+    randomUserApi: 'https://randomuser.me',
+};
