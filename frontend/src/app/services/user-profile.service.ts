3f72256743a8667f1e3e41d07986ad85eb34718a
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-04-01 19:20:09 +0100

    Update user-profile.service.ts

--- frontend/src/app/services/user-profile.service.ts
+++ frontend/src/app/services/user-profile.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth-service/auth.service';
 import { Observable } from 'rxjs';
-import { environment } from '../../env/environment';
+import { environment } from '../../../env/environment';
 
 @Injectable({
   providedIn: 'root'
