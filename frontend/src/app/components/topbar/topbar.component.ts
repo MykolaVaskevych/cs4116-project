9ef7ad98a5bb94d70e96e08ce67f65ed54a41a95
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-04-02 13:07:43 +0100

    Hide login button when user is logged in

--- frontend/src/app/components/topbar/topbar.component.ts
+++ frontend/src/app/components/topbar/topbar.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth-service/auth.service';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 import { CommonUtilsService } from '../../services/common-utils/common-utils.service';
 
 @Component({
   selector: 'app-topbar',
   imports: [
-    NgbTooltip
+    NgbTooltip,
+    CommonModule
   ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
@@ -14,15 +16,20 @@ import { CommonUtilsService } from '../../services/common-utils/common-utils.ser
 export class TopbarComponent implements OnInit {
   current_user_login: any;
   current_user_role: any;
+  isLoggedIn: boolean = false;
 
   constructor(private authService: AuthService, private commonUtilsService: CommonUtilsService) {
   }
 
   ngOnInit(): void {
     const current_user = this.commonUtilsService.getCurrentUser();
-    this.current_user_login = current_user.username;
-    this.current_user_role = current_user.role;
+    if (current_user) {
+      this.current_user_login = current_user.username;
+      this.current_user_role = current_user.role;
+      this.isLoggedIn = true;
+    }
   }
+  
   logOut(): void {
     this.authService.logOut();
   }
