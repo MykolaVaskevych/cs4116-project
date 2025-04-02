925788128ee8141c89a225581b176c1eb2b48081
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-04-02 13:03:44 +0100

    Added about us draft

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { CommonUtilsService } from '../../services/common-utils/common-utils.service';
-import { jwtDecode } from 'jwt-decode';
 import { AuthService } from '../../services/auth-service/auth.service';
+
 @Component({
   selector: 'app-about',
   imports: [],
@@ -9,19 +8,10 @@ import { AuthService } from '../../services/auth-service/auth.service';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  constructor(private authService: AuthService) {
-  }
-  clickMeButton(): void {
-    alert('You Clicked Me !!');
-
-    let accessToken = this.authService.getJWT();
-    console.log(accessToken);
-
-    let decode = jwtDecode(accessToken);
-    console.log(decode);
-
-
-    // @ts-ignore
-    console.log(Date.now() > decode.exp);
+  constructor(private authService: AuthService) {}
+  
+  contactUs(): void {
+    // You can replace this with actual contact form functionality
+    alert('Thank you for your interest! Our contact form will be available soon.');
   }
 }
